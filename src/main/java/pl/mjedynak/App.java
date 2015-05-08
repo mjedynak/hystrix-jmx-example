4b92a00427e55ef9514bebd3c8a17815a371e889
Michal Jedynak <m.jedynak@gmail.com>
2015-05-08 20:55:09 +0200

    libs update

--- src/main/java/pl/mjedynak/App.java
+++ src/main/java/pl/mjedynak/App.java
@@ -6,16 +6,18 @@ import pl.mjedynak.service.DefaultTimeService;
 import pl.mjedynak.service.HystrixTimeService;
 import pl.mjedynak.service.TimeService;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         HystrixPlugins.getInstance().registerMetricsPublisher(HystrixServoMetricsPublisher.getInstance());
 
         TimeService timeService = new HystrixTimeService(new DefaultTimeService());
         LocalDateTime currentTime = timeService.getCurrentTime();
 
         System.out.println(currentTime);
+        System.in.read();
     }
 }
